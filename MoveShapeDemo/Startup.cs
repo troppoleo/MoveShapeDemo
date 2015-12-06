53206fefc55606790f267c15a4b207669e1e6768
troppoleo <troppoleo@gmail.com>
2015-12-06 11:11:29 +0100

    Move Shape Demo

---
+++ MoveShapeDemo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MoveShapeDemo.Startup))]
+
+namespace MoveShapeDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
